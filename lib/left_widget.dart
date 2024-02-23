7e170fb82aa4fe83b07fcf3d46a91440ec846522
严浩 <me@yanhao.ren>
2024-02-23 16:21:19 +0800

    DeviceInfoPlugin

---
+++ lib/left_widget.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class LeftWidget extends StatelessWidget {
+  const LeftWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+    return Card(
+      elevation: 0,
+      color: colorScheme.surfaceVariant.withOpacity(0.3),
+      child: const Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            LeftItem(title: 'frp server', subtitle: 'frp.oo1.dev'),
+            LeftItem(title: 'frpc version', subtitle: '0.0.1'),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class LeftItem extends StatelessWidget {
+  final String title;
+  final String subtitle;
+
+  const LeftItem({
+    super.key,
+    required this.title,
+    required this.subtitle,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 0,
+      shape: RoundedRectangleBorder(
+        side: BorderSide(
+          color: Theme.of(context).colorScheme.outline.withOpacity(0.3),
+        ),
+        borderRadius: const BorderRadius.all(Radius.circular(2)),
+      ),
+      child: Container(
+        padding: const EdgeInsets.all(4),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
+              style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                    fontWeight: FontWeight.normal,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+            ),
+            Text(
+              subtitle,
+              style: Theme.of(context).textTheme.bodyMedium,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:frp_http_client/left_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -35,20 +38,65 @@ class _MyHomePageState extends State<MyHomePage> {
 
     return Scaffold(
       body: Row(
+        crossAxisAlignment: CrossAxisAlignment.end,
         children: <Widget>[
-          const Padding(
-            padding: EdgeInsets.all(16),
-            child: SizedBox(
-              width: 256,
-              child: Text('left side content'),
-            ),
+          const SizedBox(
+            width: 256 - 56,
+            child: LeftWidget(),
           ),
           Expanded(
             child: Card(
               elevation: 0,
               color: colorScheme.surfaceVariant.withOpacity(0.3),
-              child: const Center(
-                child: Text('right side content'),
+              child: Center(
+                child: ElevatedButton(
+                  onPressed: () async {
+                    String? content;
+                    final deviceInfo = DeviceInfoPlugin();
+                    if (Platform.isMacOS) {
+                      final macOsInfo = await deviceInfo.macOsInfo;
+                      debugPrint('macOsInfo: $macOsInfo');
+                      debugPrint('macOsInfo.arch: ${macOsInfo.arch}');
+                      // {"arch":"arm64","majorVersion":14,"kernelVersion":"Darwin Kernel Version 23.3.0: Wed Dec 20 21:30:44 PST 2023; root:xnu-10002.81.5~7/RELEASE_ARM64_T6000","osRelease":"Version 14.3 (Build 23D56)","cpuFrequency":0,"activeCPUs":10,"model":"MacBookPro18,2","memorySize":34359738368,"systemGUID":"DF772E83-CFAF-5284-9A05-8F65438A657F","computerName":"严浩的MacBook Pro","patchVersion":0,"minorVersion":3,"hostName":"Darwin"}
+                      content = jsonEncode(macOsInfo.data);
+                    }
+                    if (Platform.isWindows) {
+                      final windowsInfo = await deviceInfo.windowsInfo;
+                      debugPrint('windowsInfo: $windowsInfo');
+                      debugPrint(
+                          'windowsInfo.computerName: ${windowsInfo.computerName}');
+                      content = jsonEncode(windowsInfo.data);
+                    }
+
+                    if (context.mounted) {
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: const Text('DeviceInfo'),
+                            content: TextField(
+                              controller: TextEditingController(text: content),
+                              maxLines: 10,
+                              readOnly: true,
+                              decoration: const InputDecoration(
+                                border: OutlineInputBorder(),
+                              ),
+                            ),
+                            actions: <Widget>[
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: const Text('OK'),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    }
+                  },
+                  child: const Text('Click me'),
+                ),
               ),
             ),
           ),
